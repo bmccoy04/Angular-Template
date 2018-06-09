c46dc4082de7b15f7fbee36374310e022f63e386
bmccoy04 <bryanmccoy04@gmail.com>
2018-06-09 17:09:27 -0500

    created a nav module.

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,40 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import {NavModule} from './navigation/nav-module/nav.module';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './navigation/nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { NavBarComponent } from './navigation/nav-bar/nav-bar.component';
 import { BodyContainerComponent } from './body-container/body-container.component';
 import { SiteHeaderComponent } from './site-header/site-header.component';
-import { NavCardComponent } from './navigation/nav-card/nav-card.component';
+import { NavBarComponent } from './navigation/nav-bar/nav-bar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
-    NavBarComponent,
     BodyContainerComponent,
     SiteHeaderComponent,
-    NavCardComponent
+    NavBarComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'Counter', component: CounterComponent },
-      { path: 'Fetch-Data', component: FetchDataComponent },
-    ])
+    NavModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ ClientApp/src/app/navigation/nav-module/nav.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from '../../home/home.component';
+import { CounterComponent } from '../../counter/counter.component';
+import { FetchDataComponent } from '../../fetch-data/fetch-data.component';
+
+import { NavMenuComponent } from '../nav-menu/nav-menu.component';
+import { NavCardComponent } from '../nav-card/nav-card.component';
+
+
+const routes: Routes = [
+{ path: '', component: HomeComponent, pathMatch: 'full' },
+{ path: 'Counter', component: CounterComponent },
+{ path: 'Fetch-Data', component: FetchDataComponent },
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  declarations: [
+    HomeComponent,
+    CounterComponent,
+    FetchDataComponent,
+    NavMenuComponent,
+    NavCardComponent
+  ]
+})
+export class NavModule { }
